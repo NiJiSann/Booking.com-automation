8f2cce52321b641ec04cba9e375242c8e33b4a4e
Sanjar Abrorxojaev <sabrorxojaev@gmail.com>
2024-02-06 22:19:13 +0500

    Refactor

--- final_project/steps/ChangeCurrencySteps.py
+++ final_project/steps/ChangeCurrencySteps.py
@@ -4,6 +4,8 @@ from final_project.steps.BookAttractionSteps import AttractionSteps
 from final_project.pages.CommopPage import CommonPage as cp
 from final_project.pages.ChangeCurrencyPage import ChangeCurrencyPage as ccp
 from final_project.steps.common_actions import Common
+from final_project.steps.airport_taxi_step import AirportTaxiStep
+from final_project.steps.car_rental_step import CarRentalStep
 
 
 class ChangeCurrencySteps(Common):
@@ -47,15 +49,19 @@ class ChangeCurrencySteps(Common):
         return 'Currencies are not matching'
 
     def get_car_rental_price_currency(self, match) -> str:
-        time.sleep(1)
-        self.click(cp.CAR_RENTAL)
-        time.sleep(1)
-        input_loc = self.wait_for(ccp.PICK_UP_LOCATION_INPUT)
-        input_loc.send_keys('Florida')
-        time.sleep(5)
-        input_loc.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.click(ccp.SEARCH_CAR)
+        crs = CarRentalStep(self.driver)
+        crs.open_rental_page()
+        crs.enter_pic_up_location('Florida')
+        crs.click_search_button()
+        # time.sleep(1)
+        # self.click(cp.CAR_RENTAL)
+        # time.sleep(1)
+        # input_loc = self.wait_for(ccp.PICK_UP_LOCATION_INPUT)
+        # input_loc.send_keys('Florida')
+        # time.sleep(5)
+        # input_loc.send_keys(Keys.ENTER)
+        # time.sleep(1)
+        # self.click(ccp.SEARCH_CAR)
         time.sleep(20)
         source = self.driver.page_source
         if source.count(match[0]) > 1 or source.count(match[1]) > 1:
@@ -63,18 +69,23 @@ class ChangeCurrencySteps(Common):
         return 'Currencies are not matching'
 
     def get_taxi_price_currency(self, match) -> str:
-        time.sleep(1)
-        self.click(cp.AIRPORT_TAXI)
-        time.sleep(1)
-        self.wait_for(ccp.FROM_LOCATION_INPUT).send_keys('Haneda')
-        time.sleep(4)
-        self.click(ccp.SELECT_LINK)
-        time.sleep(1)
-        self.wait_for(ccp.TO_LOCATION_INPUT).send_keys('Shibuya')
-        time.sleep(4)
-        self.click(ccp.SELECT_LINK)
-        time.sleep(1)
-        self.click(ccp.SEARCH_TAXI)
+        ats = AirportTaxiStep(self.driver)
+        ats.open_airport_taxi_page()
+        ats.enter_pick_up_location('Haneda')
+        ats.enter_destination_location('Shibuya')
+        ats.click_search_button()
+        # time.sleep(1)
+        # self.click(cp.AIRPORT_TAXI)
+        # time.sleep(1)
+        # self.wait_for(ccp.FROM_LOCATION_INPUT).send_keys('Haneda')
+        # time.sleep(4)
+        # self.click(ccp.SELECT_LINK)
+        # time.sleep(1)
+        # self.wait_for(ccp.TO_LOCATION_INPUT).send_keys('Shibuya')
+        # time.sleep(4)
+        # self.click(ccp.SELECT_LINK)
+        # time.sleep(1)
+        # self.click(ccp.SEARCH_TAXI)
         time.sleep(10)
         source = self.driver.page_source
         if source.count(match[0]) > 1 or source.count(match[1]) > 1:
