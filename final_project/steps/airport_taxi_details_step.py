8e2858e45dd63ca1f7ce0dc72207ce4ea39e663c
Mexriddin Maxkamtayev <maxkamtayev@gmail.com>
2024-02-06 08:24:01 +0500

    refactoring code

--- final_project/steps/airport_taxi_details_step.py
+++ final_project/steps/airport_taxi_details_step.py
@@ -6,11 +6,11 @@ from final_project.steps.my_common_actions import MyCommonActions
 
 class AirportTaxiDetailsStep(MyCommonActions, AirportTaxiDetailsPage):
     def select_car_type(self):
-        self.scroll_to_elem(self.SECOND_CAR_TYPE)
+        self.go_to_element(self.SECOND_CAR_TYPE)
         self.click(self.SECOND_CAR_TYPE)
 
     def add_request_child_seat(self):
-        self.scroll_to_elem(self.ADD_REQUEST_CHILD_SEAT)
+        self.go_to_element(self.ADD_REQUEST_CHILD_SEAT)
         self.click(self.ADD_REQUEST_CHILD_SEAT)
         self.click(self.PLUS_CHILD_SEAT)
         self.click(self.CONFIRM_BTN_CHILD_SEAT)
@@ -22,13 +22,13 @@ class AirportTaxiDetailsStep(MyCommonActions, AirportTaxiDetailsPage):
             return False
 
     def add_comment_for_driver(self, comment):
-        self.scroll_to_elem(self.COMMENT_TEXT_ARIA)
+        self.go_to_element(self.COMMENT_TEXT_ARIA)
         self.fill(self.COMMENT_TEXT_ARIA, comment)
 
     def comment_value(self):
         return self.get_value_element(self.COMMENT_TEXT_ARIA)
 
     def click_continue_button(self):
-        self.scroll_to_elem(self.CONTINUE_BTN)
+        self.go_to_element(self.CONTINUE_BTN)
         self.click(self.CONTINUE_BTN)
         time.sleep(2)
