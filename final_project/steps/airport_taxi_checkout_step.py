7f4de85892f826bbe48d1f812df3fa7961e13f60
Mexriddin Maxkamtayev <maxkamtayev@gmail.com>
2024-02-07 13:30:36 +0500

    refactoring

--- final_project/steps/airport_taxi_checkout_step.py
+++ final_project/steps/airport_taxi_checkout_step.py
@@ -1,81 +1,103 @@
 import time
-
+import allure
 from selenium.webdriver.support.select import Select
-
 from final_project.pages.airport_taxi_page import AirportTaxiCheckoutPage
 from final_project.steps.my_common_actions import MyCommonActions
 
 
 class AirportTaxiCheckoutStep(MyCommonActions, AirportTaxiCheckoutPage):
 
+    @allure.step("Assert that airport taxi checkout page is opened")
     def taxi_checkout_page_is_opened(self):
         return self.find(self.TAXI_CHECKOUT_TITLE).is_displayed()
 
+    @allure.step("Fill first name: {first_name}")
     def enter_first_name(self, first_name):
         self.fill(self.FIRST_NAME, first_name)
 
+    @allure.step("Assert that first name is filled")
     def get_first_name_value(self):
         return self.get_value_element(self.FIRST_NAME)
 
+    @allure.step("Fill last name: {last_name}")
     def enter_last_name(self, last_name):
         self.fill(self.LAST_NAME, last_name)
 
+    @allure.step("Assert that last name is filled")
     def get_last_name_value(self):
         return self.get_value_element(self.LAST_NAME)
 
+    @allure.step("Fill email: {email}")
     def enter_email(self, email):
         self.fill(self.EMAIL, email)
 
+    @allure.step("Assert that email is filled")
     def get_email_value(self):
         return self.get_value_element(self.EMAIL)
 
+    @allure.step("Select phone code: {code}")
     def select_phone_code(self, code):
         code_select = Select(self.find(self.PHONE_CODE_SELECT))
         code_select.select_by_value(code)
 
+    @allure.step("Assert that phone code is selected")
     def get_phone_code_value(self):
         code_select = Select(self.find(self.PHONE_CODE_SELECT))
         return code_select.first_selected_option.text.split(" ")[1]
 
+    @allure.step("Fill phone number: {number}")
     def enter_phone_number(self, number):
         self.fill(self.PHONE_NUMBER, number)
 
+    @allure.step("Assert that phone number is filled")
     def get_phone_number_value(self):
         return self.get_value_element(self.PHONE_NUMBER)
 
+    @allure.step("Fill cardholder's name: {name}")
     def enter_name(self, name):
         self.go_to_element(self.CHECKOUT_SUBMIT_BUTTON)
         self.driver.switch_to.frame(self.find(self.PAYMENT_IFRAME))
         self.find(self.NAME).send_keys(name)
 
+    @allure.step("Assert that card holder's name is filled")
     def get_name_value(self):
         return self.get_value_element(self.NAME)
 
+    @allure.step("Fill card number: {card_number}")
     def enter_card_number(self, card_number):
         self.fill(self.CARD_NUMBER, card_number)
 
+    @allure.step("Assert that card number is filled")
     def get_card_number_value(self):
         return self.get_value_element(self.CARD_NUMBER)
 
-    def select_card_type(self):
+    @allure.step("Select card type: {card_type}")
+    def select_card_type(self, card_type):
         self.click(self.CARD_TYPE)
-        self.click(self.MASTER_CARD)
+        self.js_click(self.MASTER_CARD)
 
+    @allure.step("Assert that card type is selected")
     def get_card_type_value(self):
+        time.sleep(10)
         return self.get_text(self.CARD_TYPE_VALUE)
 
+    @allure.step("Fill expiry date: {expiration_date}")
     def enter_expiration_date(self, expiration_date):
         self.fill(self.EXPIRY_DATE, expiration_date)
 
+    @allure.step("Assert that expiry date is filled")
     def get_expiration_date_value(self):
         return self.get_value_element(self.EXPIRY_DATE)
 
+    @allure.step("Fill cvc card number: {cvc_number}")
     def enter_cvc_number(self, cvc_number):
         self.fill(self.CVC, cvc_number)
 
+    @allure.step("Assert that cvc number is filled")
     def get_cvc_number_value(self):
         return self.get_value_element(self.CVC)
 
+    @allure.step("Assert that 'Book and Pay' button is clickable")
     def book_and_pay_btn_is_clickable(self):
         self.driver.switch_to.default_content()
         return self.find(self.CHECKOUT_SUBMIT_BUTTON).is_enabled()
--- tests/test_car_rental.py
+++ tests/test_car_rental.py
@@ -1,77 +1,96 @@
-import time
-
 from final_project.steps.car_rental_step import CarRentalStep
 from final_project.steps.car_rental_result_search_step import CarRentalSearchResultStep
 from final_project.data.car_rental_data import CarRentalData
+from final_project.data.CommonData import Urls
 from assertpy import assert_that, soft_assertions
+import allure
+from API.GSpread.allure_report_texts import Table as report_text_sheet
 
 
+@allure.feature(report_text_sheet.get_value("car_rental"))
 class TestCarRental:
+    @allure.title(report_text_sheet.get_value("car_rental_title"))
+    @allure.description(report_text_sheet.get_value("car_rental_desc"))
+    @allure.tag("E2E")
+    @allure.tag("UI")
     def test_end_to_end_car_rental_with_drop_off_different_location(self, driver_undetected):
         car_rental_step = CarRentalStep(driver_undetected)
+        car_rental_data = CarRentalData()
         car_rental_search_result = CarRentalSearchResultStep(driver_undetected)
 
-
-        # Steps in main page
-        car_rental_step.open_page("https://www.booking.com")
-        with soft_assertions():
-            assert_that(car_rental_step.main_page_is_opened()).is_true()
-        car_rental_step.close_dialog_modal()
-        car_rental_step.change_currency()
-        with soft_assertions():
-            assert_that(car_rental_step.current_currency()).is_equal_to("USD")
+        with allure.step("Preconditions"):
+            with allure.step(f"Open the Home page of '{Urls.HOME_URL}'."):
+                car_rental_step.open_page(Urls.HOME_URL)
+                car_rental_step.close_dialog_modal()
+                with soft_assertions():
+                    assert_that(car_rental_step.main_page_is_opened()).is_true()
+            with allure.step("Change currency to usd"):
+                car_rental_step.change_currency()
+                with soft_assertions():
+                    assert_that(car_rental_step.current_currency()).is_equal_to("USD")
 
         # Steps in car rental interface
-        car_rental_data = CarRentalData()
-        car_rental_step.open_rental_page()
-        car_rental_step.change_language_to_en()
-        with soft_assertions():
-            assert_that(car_rental_step.car_rental_is_opened()).is_true()
-            assert_that(car_rental_step.search_form_is_visible()).is_true()
+        with allure.step("Open Car Rental Interface"):
+            car_rental_step.open_rental_page()
+            car_rental_step.change_language_to_en()
+            with soft_assertions():
+                assert_that(car_rental_step.car_rental_is_opened()).is_true()
+                assert_that(car_rental_step.search_form_is_visible()).is_true()
 
-        car_rental_step.check_drop_car_different_location()
-        with soft_assertions():
-            assert_that(car_rental_step.is_checked_drop_car()).is_true()
+        with allure.step("Check checkbox drop off at different location"):
+            car_rental_step.check_drop_car_different_location()
+            with soft_assertions():
+                assert_that(car_rental_step.is_checked_drop_car()).is_true()
 
-        car_rental_step.uncheck_driver_age_between()
-        with soft_assertions():
-            assert_that(car_rental_step.is_checked_age_between()).is_false()
-            assert_that(car_rental_step.age_input_is_visible()).is_true()
+        with allure.step("Default Age checkbox is uncheck"):
+            car_rental_step.uncheck_driver_age_between()
+            with soft_assertions():
+                assert_that(car_rental_step.is_checked_age_between()).is_false()
+                assert_that(car_rental_step.age_input_is_visible()).is_true()
 
-        car_rental_step.enter_driver_age(car_rental_data.age)
-        with soft_assertions():
-            assert_that(car_rental_data.age).is_equal_to(car_rental_step.age_value())
+        with allure.step("Enter driver's age"):
+            car_rental_step.enter_driver_age(car_rental_data.age)
+            with soft_assertions():
+                assert_that(car_rental_data.age).is_equal_to(car_rental_step.age_value())
 
-        car_rental_step.enter_pic_up_location(car_rental_data.pic_up_location)
-        with soft_assertions():
-            assert_that(car_rental_step.pic_up_location_value()).contains(car_rental_data.pic_up_location)
+        with allure.step("Enter Pick-up location"):
+            car_rental_step.enter_pic_up_location(car_rental_data.pic_up_location)
+            with soft_assertions():
+                assert_that(car_rental_step.pic_up_location_value()).contains(car_rental_data.pic_up_location)
 
-        car_rental_step.enter_drop_off_location(car_rental_data.drop_off_location)
-        with soft_assertions():
-            assert_that(car_rental_step.drop_off_location_value()).contains(car_rental_data.drop_off_location)
-        car_rental_step.select_date_and_time(car_rental_data.pick_up_date,
-                                             car_rental_data.pick_up_time, "pick_up")
-        with soft_assertions():
-            assert_that(car_rental_step.selected_pick_up_date()).is_in(car_rental_data.pick_up_date)
-            assert_that(car_rental_data.pick_up_time).contains(car_rental_step.selected_pick_up_time())
+        with allure.step("Enter Drop-off location"):
+            car_rental_step.enter_drop_off_location(car_rental_data.drop_off_location)
+            with soft_assertions():
+                assert_that(car_rental_step.drop_off_location_value()).contains(car_rental_data.drop_off_location)
 
-        car_rental_step.select_date_and_time(car_rental_data.drop_off_date,
-                                             car_rental_data.drop_off_time, "drop_off")
-        with soft_assertions():
-            assert_that(car_rental_step.selected_drop_off_date()).is_in(car_rental_data.drop_off_date)
-            assert_that(car_rental_data.drop_off_time).contains(car_rental_step.selected_drop_off_time())
+        with allure.step("Enter Pick-up date and time"):
+            car_rental_step.select_date_and_time(car_rental_data.pick_up_date,
+                                                 car_rental_data.pick_up_time, "pick_up")
+            with soft_assertions():
+                assert_that(car_rental_step.selected_pick_up_date()).is_in(car_rental_data.pick_up_date)
+                assert_that(car_rental_data.pick_up_time).contains(car_rental_step.selected_pick_up_time())
 
-        car_rental_step.click_search_button()
-        with soft_assertions():
-            assert_that(car_rental_search_result.car_rental_result_search_is_opened()).is_true()
+        with allure.step("Enter Drop-off date and time"):
+            car_rental_step.select_date_and_time(car_rental_data.drop_off_date,
+                                                 car_rental_data.drop_off_time, "drop_off")
+            with soft_assertions():
+                assert_that(car_rental_step.selected_drop_off_date()).is_in(car_rental_data.drop_off_date)
+                assert_that(car_rental_data.drop_off_time).contains(car_rental_step.selected_drop_off_time())
+
+        with allure.step("Submit car rental search"):
+            car_rental_step.click_search_button()
+            with soft_assertions():
+                assert_that(car_rental_search_result.car_rental_result_search_is_opened()).is_true()
 
 
         # Steps in car rental search result page
-        car_rental_search_result.select_car_type()
-        with soft_assertions():
-            assert_that(car_rental_search_result.is_selected_car_type()).is_true()
+        with allure.step("Select premium car type"):
+            car_rental_search_result.select_car_type()
+            with soft_assertions():
+                assert_that(car_rental_search_result.is_selected_car_type()).is_true()
 
-        car_rental_search_result.select_first_car()
-        with soft_assertions():
-            assert_that(car_rental_search_result.car_deal_page_is_opened()).is_true()
+        with allure.step("View deal"):
+            car_rental_search_result.select_first_car()
+            with soft_assertions():
+                assert_that(car_rental_search_result.car_deal_page_is_opened()).is_true()
 
